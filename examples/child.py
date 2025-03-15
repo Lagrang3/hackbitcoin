7d6e672c2e5e353470294867d33dd965111109d1
Lagrang3 <lagrang3@protonmail.com>
2025-03-15 09:06:44 +0100

    Change example child to be terminal friendly
    Signed-off-by: Lagrang3 <lagrang3@protonmail.com>

--- examples/child.py
+++ examples/child.py
@@ -1,31 +1,55 @@
 #!/usr/bin/env python
+import sys
 from hackbitcoin.bip39 import Mnemonic
 from hackbitcoin.extkey import ExtendedKey
 from hackbitcoin.bip85 import BIP85
 
+def show_help():
+    print("Usage: child.py <mnemonic> <passphrase> <length of child (12 or 24)> <index of child (0 to 2147483647)>")
+    sys.exit(1)
+
+def check_nwords(nwords):
+    return nwords=='12' or nwords=='24'
+
+def check_index(index):
+    try:
+        i = int(index)
+        if i<0 or i>2147483647:
+            return False
+    except:
+        return False
+    return True
+
+def check_passphrase(passphrase):
+    return True
+
+def check_mnemonic(mnemonic):
+    return Mnemonic.is_valid(mnemonic)
 
 if __name__=="__main__":
 
-    mnemonic = input("Enter the parent mnemonic: ")
-    print()
+    if len(sys.argv) != 5:
+        show_help()
 
-    passphrase = input("Passphrase (can be empty): ")
-    print('Passphrase is:"{}"'.format(passphrase))
+    mnemonic = sys.argv[1]
+    if not check_mnemonic(mnemonic):
+        show_help()
+
+    passphrase = sys.argv[2]
+    if not check_passphrase(passphrase):
+        show_help()
 
     xpriv = Mnemonic.master_key(mnemonic.strip(), passphrase)
 
-    print()
-
-    nwords = input("Length of child mnemonic sentence (12 or 24): ")
-    if not( nwords=='12' or nwords=='24'):
-        raise ValueError('you must enter either 12 or 24')
+    nwords = sys.argv[3]
+    if not check_nwords(nwords):
+        show_help()
     nwords = int(nwords)
 
-    print()
-
-    index = input("Index of child mnemonic sentence (0 to 2147483647): ")
-
-    print()
+    index = sys.argv[4]
+    if not check_index(index):
+        show_help()
+    index = int(index)
 
     result = BIP85.app_mnemonic(xpriv, nwords, index)
-    print("Child mnemonic is:", result)
+    print(result)
